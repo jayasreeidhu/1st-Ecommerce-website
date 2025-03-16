71c0e7fcb5e11915a13b4921c5bb1b0f9b8fcc22
jayasreeidhu <jayasreeidhunov@gmail.com>
2025-03-16 19:10:57 +0530

    Create cart_extras.py
    cart_extras.py

---
+++ Ecommerce/products/templatetags/cart_extras.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def multiply(value, arg):
+    try:
+        return value * arg
+    except (ValueError, TypeError):
+        return ''
+    
+    templatetags
